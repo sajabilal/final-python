d7f44b0abf0b96ec4b025e10a2fc9f5b7fbeed5f
sajabilal <sajabilal69@gmail.com>
2024-08-25 16:02:16 +0200

    added sudo to start docker

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker pull saja123/final-python:latest
-docker build -t final-python .
-docker run -p 5000:5000 final-python 
+sudo docker pull saja123/final-python:latest
+sudo docker build -t final-python .
+sudo docker run -p 5000:5000 final-python 
