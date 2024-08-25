0ba3e52b5c8f019867752a18b342b35b186aca8d
sajabilal <sajabilal69@gmail.com>
2024-08-25 15:57:20 +0200

    updated pull command

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo systemctl start docker
-sudo systemctl enable docker
-docker image pull final-python 
+docker pull saja123/final-python:latest
 docker build -t final-python .
 docker run -p 5000:5000 final-python 
