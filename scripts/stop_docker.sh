88bc7a3cbd8cd8ca1c3294551274358212dd3a5f
sajabilal <sajabilal69@gmail.com>
2024-08-27 09:24:21 +0200

    still the same editing

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 IMAGE_NAME="saja123/final-python"
 CONTAINER_ID=$(sudo docker ps -q --filter "ancestor=$IMAGE_NAME")
-sudo docker stop saja123/final-python || true
-sudo docker rm saja123/final-python || true
+sudo docker stop $CONTAINER_ID || true
+sudo docker rm $CONTAINER_ID || true
