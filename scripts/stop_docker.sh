cb6b93ce7e5a13078cc7abcdf583cf0c24663c97
sajabilal <sajabilal69@gmail.com>
2024-08-27 09:21:41 +0200

    stop script edit

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo docker stop saja123/final-python 
-sudo docker rm saja123/final-python 
+IMAGE_NAME="saja123/final-python"
+CONTAINER_ID=$(sudo docker ps -q --filter "ancestor=$IMAGE_NAME")
+sudo docker stop saja123/final-python || true
+sudo docker rm saja123/final-python || true
