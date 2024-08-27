f2fe3f7fe496b09e9ff9146f4e8dd6400bbc3349
sajabilal <sajabilal69@gmail.com>
2024-08-27 07:26:59 +0200

    EDITED START DOCKER SCRIPT

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo su - ec2-user -c "docker pull saja123/final-python"
+sudo docker pull saja123/final-python
 sudo docker build -t final-python .
 sudo docker run -p 5000:5000 final-python 
