b1a10ea6a228fd3523a04dbb1bbcd380aff70281
sajabilal <sajabilal69@gmail.com>
2024-08-30 11:17:32 +0200

    edited stop script

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PORT="5000"
 CONTAINER_ID=$(sudo docker ps -q --filter "publish=$PORT")
-sudo docker stop $CONTAINER_ID 
-sudo docker rm $CONTAINER_ID 
+sudo docker stop $CONTAINER_ID || true
+sudo docker rm $CONTAINER_ID  || true
